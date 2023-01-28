73fb0035aef032eb16b0cf4ca41b8b3055a4c6f5
TT-RR <kaiza1mage@gmail.com>
2023-01-28 13:05:37 +0900

    create newfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:twitter/time_line/time_line_page.dart';
+import 'package:twitter/view/time_line/time_line_page.dart';
 
 void main() {
   runApp(const MyApp());
