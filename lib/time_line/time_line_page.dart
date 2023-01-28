75222593d0cf1f079af0596e5b351ce4a7fc761d
TT-RR <kaiza1mage@gmail.com>
2023-01-28 10:25:35 +0900

    Create new project

---
+++ lib/time_line/time_line_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TimeLinePage extends StatefulWidget {
+  const TimeLinePage({Key? key}) : super(key: key);
+
+  @override
+  State<TimeLinePage> createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('タイムラインページ'),
+    );
+  }
+}
